4b1a5dc612d07dab7e51ee08905ad670ce4a8e27
genius-0963 <geniusthegreat0963@gmail.com>
2026-06-30 23:20:55 +0530

    feat(config): Add config module initialization

---
+++ modelx_voice/config/__init__.py
@@ -0,0 +1,14 @@
+from .manager import ConfigManager, ModelXConfig, APIConfig, VoiceConfig, AudioConfig, BehaviorConfig
+from .setup_wizard import SetupWizard, run_setup_wizard, quick_setup
+
+__all__ = [
+    "ConfigManager",
+    "ModelXConfig", 
+    "APIConfig",
+    "VoiceConfig", 
+    "AudioConfig",
+    "BehaviorConfig",
+    "SetupWizard",
+    "run_setup_wizard",
+    "quick_setup",
+]
